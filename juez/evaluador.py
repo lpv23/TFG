6cf0d857858f0f105d48f7b22f055b69e04009e9
lpv23 <laurapinavalencia@gmail.com>
2023-03-05 13:01:48 +0100

    Rutas arregladas, diccionario problema ampliado y sufijos y nombres de carpeta definidos al principio

--- juez/evaluador.py
+++ juez/evaluador.py
@@ -1,11 +1,16 @@
+import os.path
 import time
 from os import scandir
+
+sufijo_entrada = "entrada.txt"
+sufijo_salida = "salida.txt"
+carpeta_problemas = "problemas"
 
 
 # Inspeccionar el contenido de la carpeta "problemas" y devolver una lista con los nombres de las subcarpetas
 def lista_problemas():
     carpetas = []
-    with scandir('C:\\Users\\laura\\PycharmProjects\\TFG\\problemas') as problemas:
+    with scandir(carpeta_problemas) as problemas:
         for file in problemas:
             if file.is_dir():
                 carpetas.append(file.name)
@@ -21,17 +26,24 @@ def lista_problemas():
 # 'title': primera línea de problema.txt -- 'Encuentra el menor elemento'
 # 'body': segunda línea de problema.txt -- 'Encuentra el menor elemento en una lista que no está ordenada y devuélvelo'
 # 'funcion': nombre de la función que el usuario tiene que crear (tercera línea de problema.txt) -- 'menor'
+# 'aumentar': cuarta linea - nº del parámetro que debe aumentar su tamaño (si es 0, es que no hay nada que aumentar)
+# 'tipo_param': a partir de la linea 5 - tipo de cada parámetro (uno en cada línea)
+# (con un espacio para indicar el tipo dentro de una tupla o lista) (añadir sorted si está ordenada)
 def lee_problema(id):
     problemas = lista_problemas()
     for pr in problemas:
         if id == pr:
-            path = 'C:\\Users\\laura\\PycharmProjects\\TFG\\problemas\\' + id + '\\problema.txt'
+            path = os.path.join(carpeta_problemas, id, "problema.txt")
+            print(path)
             with open(path, 'r') as fichero:
-                lineas = fichero.read().split('\n')[:3]
-            return {'id': pr,
-                    'title': lineas[0],
-                    'body': lineas[1],
-                    'funcion': lineas[2]}
+                lineas = fichero.read().split('\n')
+            if len(lineas) >= 5:
+                return {'id': pr,
+                        'title': lineas[0],
+                        'body': lineas[1],
+                        'funcion': lineas[2],
+                        'aumentar': lineas[3],
+                        'tipo_param': lineas[4:]}
     return {}
 
 
@@ -52,27 +64,28 @@ def evalua_problema(id, fichero):
     funcion = lee_problema(id)['funcion']
     with open(fichero, 'r') as fichero:
         exec(fichero.read())
-    pruebas, resultados = [], ''
-    with scandir('C:\\Users\\laura\\PycharmProjects\\TFG\\problemas\\' + str(id)) as carpeta:
+    pruebas, resultados = [], []
+    with scandir(os.path.join(carpeta_problemas, id)) as carpeta:
         for file in carpeta:
-            if file.is_file() and file.name.endswith('entrada.txt'):
-                pruebas.append((file.path, file.path[:-12] + '.salida.txt'))
+            if file.is_file() and file.name.endswith(sufijo_entrada):
+                pruebas.append((file.path, file.path[:-len(sufijo_entrada)] + sufijo_salida))
     for prueba in pruebas:
         with open(prueba[0], 'r') as e:
             entrada = e.read().split('\n')
-            entrada = filter(None, entrada)
-            entrada = ', '.join(entrada)
+            if entrada[-1] == '':  # Quitamos el argumento vacío que surge si hay una línea vacía al final
+                entrada = ', '.join(entrada[:-1])
+            else:
+                entrada = ', '.join(entrada)
             tiempoini = time.perf_counter()
             salidareal = eval(funcion + '(' + entrada + ')')
             tiempototal = time.perf_counter() - tiempoini
         with open(prueba[1], 'r') as s:
             salidaesperada = eval(s.read())
         if salidareal == salidaesperada:
-            resultados += 'OK' + ', ' + str(tiempototal) + '\n'
+            resultados.append('OK' + ', ' + str(tiempototal))
         else:
-            resultados += '(' + entrada + ')' + ', ' + str(salidaesperada) + ', ' + str(salidareal) + '\n'
-    return resultados[:-1]
-
+            resultados.append('(' + entrada + ')' + ', ' + str(salidaesperada) + ', ' + str(salidareal))
+    return resultados
 
 # print(evalua_problema('alfabetica', 'C:\\Users\\laura\\PycharmProjects\\TFG\\otros\\solucion-alfabetica.py'))
 # print(evalua_problema('menorque', 'C:\\Users\\laura\\PycharmProjects\\TFG\\otros\\solucion-menorque.py'))
